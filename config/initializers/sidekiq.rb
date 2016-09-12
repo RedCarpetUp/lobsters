32da008541dd2218b4aff9bc3c1d1c19dd90cfcc
SiDevesh <sid.swapnildevesh@gmail.com>
2016-09-13 04:59:02 +0530

    Reset searchkiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 9 }
-end
+#Sidekiq.configure_server do |config|
+#  config.redis = { :size => 9 }
+#end
 
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
-end
+#Sidekiq.configure_client do |config|
+#  config.redis = { :size => 1 }
+#end
